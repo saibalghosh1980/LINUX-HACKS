2d8a1c178f34bfbd65b40d06e4dbc54f34a0b795
Saibal Kumar Ghosh <Saibal.KumarGhosh.Contractor@oup.com>
2018-10-15 19:52:59 +0100

    NA

--- UpdateApplication.py
+++ UpdateApplication.py
@@ -1,6 +1,6 @@
-with open('./application.yml', 'r') as original:
+with open('./src/main/resources/application.yml', 'r') as original:
     data = original.read()
     with open('./hawtio.txt', 'r') as hawtio:
         dataToWrite = hawtio.read()
-        with open('./application.yml', 'w') as modified:
+        with open('./src/main/resources/application.yml', 'w') as modified:
             modified.write(dataToWrite + data)
