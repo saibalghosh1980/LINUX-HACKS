c5e7c5f706f8af681de27e8b750b1848a4201a82
Saibal Kumar Ghosh <Saibal.KumarGhosh.Contractor@oup.com>
2018-10-22 22:04:08 +0100

    For HAWTIO

--- CallEureka.py
+++ CallEureka.py
@@ -8,22 +8,28 @@ headers = {
     'cache-control': "no-cache"
 }
 response = requests.get(
-    'http://gboxfdockdev001.uk.oup.com:8761/eureka/apps', headers=headers)
+    'http://gboxfdockprd001.uk.oup.com:8761/eureka/apps', headers=headers)
 data = response.json()
 for majorkey, subdict in data["applications"].items():
     # print(majorkey)
     if majorkey == 'application':
         # print(subdict[0]["name"])
         for item in subdict:
-            print(item["name"])
+            #print(item["name"])
             data = {}
-            data['name'] = ''.join(['dev-',item["name"]])
+            data['name'] = ''.join(['prod-',item["name"]])
             data['scheme'] = "http"
-            data['host'] = "gboxfdockdev001.uk.oup.com"
+            data['host'] = "gboxfdockprd001.uk.oup.com"
             data['port'] = 10080
             data['path'] = ''.join(["/",item["name"], "/hawtio/jolokia"])           
             allJson.append(data)
 json_data= json.dumps(allJson)
 
-print(json_data)
+print(json.dumps(json_data))
+with open('./OpenHAWT.temp', 'r') as original:
+    data = original.read()
+    data = data.replace("TEMP_PLACHOLDER",json.dumps(json_data))
+    with open('./OpenHAWTIO.html', 'w') as modified:
+            modified.write(data)
+
 # print(data["applications"]["application"][0]["name"])
